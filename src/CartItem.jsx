009ce3ca5b38c278cdd7c2f196e4b3294b29728e
binta1112 <tbinta1112@gmail.com>
2024-10-31 14:12:00 +0100

    all logics for the cart contains

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
@@ -6,30 +6,53 @@ import './CartItem.css';
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
-
+  
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+   let total = 0;
+   cart.forEach(item => {
+    total += parseFloat(item.cost.match(/[0-9]+/)[0])*item.quantity;
+   });
+   return total;
   };
 
   const handleContinueShopping = (e) => {
+    onContinueShopping(e);
    
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({...item,quantity:item.quantity+1}))
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity > 1)
+   {
+    dispatch(updateQuantity({...item,quantity:item.quantity-1}))
+
+   }
+   else
+   {
+    dispatch(removeItem(item));
+   }
   };
 
   const handleRemove = (item) => {
+    
+    dispatch(removeItem(item));
+    
   };
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let total=0;
+    total += parseFloat(item.cost.match(/[0-9]+/)[0])*item.quantity;
+    return total;
   };
 
   return (
@@ -57,12 +80,13 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1"onClick={(e)=> handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
+
 
 
